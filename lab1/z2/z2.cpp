81b97260ef95464833def5a06f0cc66a3019cddd
pkoneva <kz.konev@.mail.ru>
2018-03-28 18:49:47 +0600

    Решена  задача 2

---
+++ lab1/z2/z2.cpp
@@ -0,0 +1,14 @@
+// z2.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+	char a[100];
+	scanf("%s", a);
+	printf("My stremimsa k %s", a);
+    return 0;
+}
+
