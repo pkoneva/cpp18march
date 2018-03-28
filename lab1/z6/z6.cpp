651af97c00d06f65b8d263538139d067559e8a4e
pkoneva <kz.konev@.mail.ru>
2018-03-28 19:05:48 +0600

    Решение 5, 6 задачи

---
+++ lab1/z6/z6.cpp
@@ -0,0 +1,14 @@
+// z6.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+	char a[100], b[100];
+	scanf("%s %s", a, b);
+	printf("Mena zovut %s, %s %s", b, a, b);
+    return 0;
+}
+
